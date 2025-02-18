3af7e630911bf29f0c1751df1410545df695035d
Chouaib-Djerdi <djerdichouaib@gmail.com>
2025-02-18 15:53:09 +0100

    Features Section

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -8,7 +8,7 @@ const Tag = (props: HTMLAttributes<HTMLDivElement>) => {
             className={twMerge(
                 "inline-flex border border-lime-400 gap-2 text-lime-400 px-3 py-1 rounded-full uppercase items-center ",
                 className
-            )}
+            )} {...otherProps}
         >
             <span>&#10038;</span>
             <span className="text-sm">{children}</span>
